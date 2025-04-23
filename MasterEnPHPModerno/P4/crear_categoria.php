e09a729189299017fae51b18346e549f4d1c27fa
j-muir <j.muir@act-cs.fr>
2025-04-23 15:15:38 +0200

    P4.3

--- MasterEnPHPModerno/P4/crear_categoria.php
+++ MasterEnPHPModerno/P4/crear_categoria.php
@@ -1,4 +1,41 @@
 <?php include("includes/header.php") ?>
+
+<?php
+
+    if($_POST["crearCategoria"]){
+        //Obtener valores
+        $nombre = $_POST["nombre"];
+
+        //Validar si está vacío
+        if(empty($nombre)){
+            $error = "Error, algunos campos obligatorios están vacíos.";
+            header('Location: crear_categoria.php?error=' . $error);
+        }else{
+            //Insersión de datos en bdd
+            $fechaActual = date("Y-m-d");
+            $query = "INSERT INTO categorias (nombre, fecha_creacion) VALUES (:nombre, :fecha_creacion)";
+
+            $stmt = $pdo->prepare($query);
+
+            $stmt->bindParam(":nombre", $nombre, PDO::PARAM_STR);
+            $stmt->bindParam(":fecha_creacion", $fechaActual, PDO::PARAM_STR);
+
+            $resultado = $stmt->execute();
+
+            if($resultado){
+                $mensaje = "Categoría creada con éxito.";
+                header('Location: categorias.php?mensaje=' . $mensaje);
+                exit();
+            }else{
+                $error = "Error, no se pudo crear la categoría.";
+                header('Location: categorias.php?error=' . $error);
+                exit();
+            };
+        };
+    }
+
+?>
+
 <div class="row">
         <div class="col-sm-6">
             <h3>Crear una Nueva Categoría</h3>
