03b53e0aefa919ac0e97c1dd86acd7ae42fdcdd1
j-muir <j.muir@act-cs.fr>
2025-04-25 16:03:38 +0200

    P5.1.2

--- MasterEnPHPModerno/P5/buscar.php
+++ MasterEnPHPModerno/P5/buscar.php
@@ -5,7 +5,7 @@ include_once("conexion.php");
 $cedula =  isset($_GET['cedula']) ?  trim($_GET['cedula']) : '';
 
 $query = "SELECT * FROM empleado WHERE cedula = :cedula";
-$stmt->$pdo->prepare($query);
+$stmt = $pdo->prepare($query);
 $stmt->bindParam(":cedula", $cedula, PDO::PARAM_STR);
 $stmt->execute();
 
