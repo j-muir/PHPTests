e7b69480846118be1bf128a25130cc6a9c77934c
j-muir <j.muir@act-cs.fr>
2025-05-05 16:06:12 +0200

    P7.4

---
+++ MasterEnPHPModerno/P7/api/categorias/crear.php
@@ -0,0 +1,34 @@
+<?php
+
+//Encabezados (HEADERS)
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once '../../config/Basemysql.php';
+    include_once '../../models/Categoria.php';
+
+//Instanciamos la base de datos y conexión
+    $baseDatos = new Basemysql();
+    $db = $baseDatos->connect();
+
+//Instanciamos el objeto categoría
+    $categoria = new Categoria($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $categoria->nombre = $data->nombre;
+
+//Crear categorias
+    if($categoria->crear()){
+        echo json_encode(
+        array('message' => 'Categoría creada.')
+    );
+    }else {
+        echo json_encode(
+            array('message' => 'Categoria no creada.')
+        );
+    }
+
+?>
